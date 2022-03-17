6373d5b1b60aa56bfd3e0355a016800e187d7a01
DoHoanThien <dongochoanthien@gmail.com>
2022-03-17 11:49:46 +0700

    upload source

---
+++ js/main.js
@@ -0,0 +1,36 @@
+/* TruncateCardTitle */
+window.addEventListener("load", function () {
+  truncateCardTitle();
+});
+
+function truncateCardTitle() {
+  var cardList = document.getElementsByClassName("card-title");
+  for (var i = 0; i < cardList.length; i++) {
+    var text = cardList[i].innerHTML;
+    var newText = truncateString(text, 39);
+    cardList[i].innerHTML = newText;
+  }
+}
+
+function truncateString(str, num) {
+  if (str.length > num) {
+    return str.slice(0, num) + "...";
+  } else {
+    return str;
+  }
+}
+
+/* Sidebar Mini */
+var toggleBtn = document.querySelector(".sidebarMini__button");
+var sidebarMini = document.querySelector(".sidebarMini");
+
+toggleBtn.addEventListener("click", function () {
+  sidebarMini.classList.toggle("is-opend");
+});
+
+/* Dark Mode */
+var switchBtn = document.querySelector("#checkbox");
+
+switchBtn.addEventListener("click", function () {
+  document.querySelector("body").classList.toggle("darkMode");
+});
